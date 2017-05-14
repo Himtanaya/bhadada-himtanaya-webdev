589f19839e1f8f7d2a0d9304025972072a7fa372
himtanaya <bhadada.h@husky.neu.edu>
2017-05-14 08:15:14 -0400

    old app.js

--- test/app.js
+++ test/app.js
@@ -4,14 +4,17 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-    
-    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-        var username = process.env.MLAB_USERNAME_WEBDEV;
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
-        connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds137801.mlab.com:37801/heroku_m8dr0ld9';
-}
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
+
+
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
 
