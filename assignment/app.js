18d0e7b0402e8803b94ba4e22fa387fff561d2a4
himtanaya <bhadada.h@husky.neu.edu>
2017-06-12 23:35:59 -0400

    connection string

--- assignment/app.js
+++ assignment/app.js
@@ -1,7 +1,19 @@
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/bhadada-himtanaya-webdev');
 // mongoose.Promise = require('q').Promise;
+var connectionString = 'mongodb://127.0.0.1:27017/test';
 
+
+if(process.env.MLAB_USERNAME) {
+    connectionString = process.env.MLAB_USERNAME + ":" +
+        process.env.MLAB_PASSWORD + "@" +
+        process.env.MLAB_HOST + ':' +
+        process.env.MLAB_PORT + '/' +
+        process.env.MLAB_APP_NAME;
+}
+
+var mongoose = require("mongoose");
+mongoose.connect(connectionString);
 
 require("./services/user.service.server");
 require("./services/website.service.server");
